00fbe6d0cf8324e608ec55b93346a0dab8400539
Yangbo Liu <geeksoap@outlook.com>
2024-07-14 18:14:21 -0400

    express setup

---
+++ src/envConfig.ts
@@ -0,0 +1,8 @@
+import path from "path";
+import dotenv from "dotenv";
+
+const envPath =
+  process.env.NODE_ENV === "production" ? ".env.production" : ".env";
+dotenv.config({
+  path: path.resolve(__dirname, "../", envPath),
+});
