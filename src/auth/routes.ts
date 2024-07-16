e38ca6ad0f52aecb7435d31878f5bbe8441a3f52
Yangbo Liu <geeksoap@outlook.com>
2024-07-16 13:35:40 -0400

    signup service

--- src/auth/routes.ts
+++ src/auth/routes.ts
@@ -9,13 +9,15 @@ const userRepository = AppDataSource.getRepository(User);
 const jwtSecret = process.env.JWT_SECRET || "secret";
 
 router.post("/signup", async (req, res) => {
-  const { username, email, password } = req.body;
+  const { username, email, password, plan, api } = req.body;
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = userRepository.create({
       username,
       email,
       password: hashedPassword,
+      plan,
+      api,
     });
     await userRepository.save(user);
     res.status(201).json(user);
@@ -32,7 +34,7 @@ router.post("/login", async (req, res) => {
       const token = sign({ email: user.email, id: user._id }, jwtSecret, {
         expiresIn: "1h",
       });
-      res.json({ token });
+      res.json({ token, user });
     } else {
       res.status(401).json({ message: "Invalid email or password" });
     }
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -20,17 +20,33 @@ export class User {
     enum: ["USER", "ADMIN"],
     default: "USER",
   })
-  role: string;
+  role: string;   
+  
+  @Column({
+    type: "enum",
+    enum: ["basic", "standard", "premium"],
+    default: "basic",
+  })
+  plan: string;
+    
+  @Column()
+  api: string;
+    
+
 
   constructor(
     username: string,
     email: string,
     password: string,
-    role: string = "USER"
+    role: string = "USER",
+    plan: string,
+    api: string = '775ab391c192a98f82f683c7653a4b59'
   ) {
     this.username = username;
     this.email = email;
     this.password = password;
     this.role = role;
+    this.plan = plan;
+    this.api = api;
   }
 }
